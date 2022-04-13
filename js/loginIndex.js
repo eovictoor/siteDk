447df5fd47eb14b1756857e40db62814e0e46de4
Victor <86137531+eovictoor@users.noreply.github.com>
2022-04-12 20:40:58 -0500

    Add files via upload

--- js/loginIndex.js
+++ js/loginIndex.js
@@ -11,6 +11,29 @@ function loginPanel(){
     document.getElementById('painel2').style.display = 'flex'
 } 
 
+function clearCamps(){
+    let nickGross = document.getElementById('usernameregister').value = ''
+
+
+    let emailUser = document.getElementById('emailregister').value = ''
+
+    let cpfGross = document.getElementById('cpfregister').value = ''
+
+
+    let cepGross = document.getElementById('cepregister').value = '' 
+
+
+    let addressGross = document.getElementById('enderegister').value = '' 
+
+
+    let passwordUser = document.getElementById('passwordregister').value = '' 
+    let passwordVerify = document.getElementById('passwordverify').value = '' 
+}
+
+
+let nada = 0;
+
+
 
 function formataCPF(cpf){
     const elementoAlvo = cpf
@@ -29,14 +52,18 @@ function formataCep(cep){
     const elementoAlvo = cep
     const cepAtual = cep.value 
     let cepAtualizado;
-    
-    cepAtualizado = cepAtual.replace(/(\d{5})/,function( argumento1){
+    if(cepAtual.includes("-")){
+        nada;
+    }else{
+        cepAtualizado = cepAtual.replace(/(\d{5})/,function( argumento1){
+            return argumento1 + '-';
+        })
+        elementoAlvo.value = cepAtualizado
+    }  
 
-        return argumento1 + '-';
-    })
-    elementoAlvo.value = cepAtualizado
-    }
+}
     
+
 const toBase64 = file => new Promise((resolve, reject)=>{
     const reader = new FileReader()
     reader.readAsDataURL(file)
@@ -64,7 +91,102 @@ function registerNewuser(){
     let passwordUser = document.getElementById('passwordregister').value 
     let passwordVerify = document.getElementById('passwordverify').value 
 
+    let fileresidencia = document.getElementById('imageresidencia').files[0]
+    let filetshirt = document.getElementById('imagetshirt').files[0]
+    let filedocument = document.getElementById('imagedocument').files[0]
+
+    toBase64(fileresidencia).then(res =>{
+        localStorage.setItem('arquivoresidencia',res)
+    }).then(err =>{
+        nada;
+    })
+    toBase64(filetshirt).then(res =>{
+        localStorage.setItem('arquivocamisa',res)
+    }).then(err =>{
+        nada;
+    })
+    toBase64(filedocument).then(res=>{
+        localStorage.setItem('arquivodocumento',res)
+    }).then(err =>{
+        nada;
+    })
     if(nickGross.length == 0 || emailUser.length == 0 || cpfGross.length == 0 || cepGross.length == 0 || addressGross.length == 0 || passwordUser.length == 0 || passwordVerify == 0){
-        /* vai ter mais verificacoes no front, so deu pra codar das 10:40 as 11:57, acordei tarde sem querer, e faltei colocar para guardas as imagens em base64 nas variavel, mas qnd chegar ja vou arrumar*/
+        document.getElementById('catch').innerHTML = "Verifique se todos os campos estão preenchidos!"
+        document.getElementById('catch').style.color = 'red'
+        document.getElementById('catch').style.marginTop = '15px'
+        document.getElementById('bloco3').style.height = '1050px'
+    }else{
+        if(localStorage.getItem('arquivoresidencia') == false){
+            document.getElementById('catch').innerHTML = "A imagem do comprovante de residência está faltando!"
+            document.getElementById('catch').style.color = 'red'
+            document.getElementById('catch').style.marginTop = '15px'
+            document.getElementById('bloco3').style.height = '1050px'   
+        }else{
+            if(localStorage.getItem('arquivocamisa') == false){
+                document.getElementById('catch').innerHTML = "A sua foto de camisa branca, está faltando!"
+                document.getElementById('catch').style.color = 'red'
+                document.getElementById('catch').style.marginTop = '15px'
+                document.getElementById('bloco3').style.height = '1050px'  
+            }else{
+                if(localStorage.getItem('arquivodocumento') == false){
+                    document.getElementById('catch').innerHTML = "A sua foto com o documento na mão está faltando!"
+                    document.getElementById('catch').style.color = 'red'
+                    document.getElementById('catch').style.marginTop = '15px'
+                    document.getElementById('bloco3').style.height = '1050px'  
+                }else{
+                    if(passwordUser == passwordVerify){
+                        if(cpfUser.length < 11 || cpfUser.length > 11){
+                            document.getElementById('catch').innerHTML = "Verifique seu CPF!"
+                            document.getElementById('catch').style.color = 'red'
+                            document.getElementById('catch').style.marginTop = '15px'
+                            document.getElementById('bloco3').style.height = '1050px'  
+                        }else{
+                            if(passwordVerify.length < 6){
+                                document.getElementById('catch').innerHTML = "Atenção, sua senha é muito pequena, o mínimo é seis caracteres!"
+                                document.getElementById('catch').style.color = 'red'
+                                document.getElementById('catch').style.marginTop = '15px'
+                                document.getElementById('bloco3').style.height = '1050px'  
+                            }else{
+                                if(addressUser.length < 4){
+                                    document.getElementById('catch').innerHTML = "Verifique seu CEP!"
+                                    document.getElementById('catch').style.color = 'red'
+                                    document.getElementById('catch').style.marginTop = '15px'
+                                    document.getElementById('bloco3').style.height = '1050px' 
+                                }else{
+                                    if(cepUser.length < 8 || cepUser.length > 8){
+                                        document.getElementById('catch').innerHTML = "Verifique seu CEP!"
+                                        document.getElementById('catch').style.color = 'red'
+                                        document.getElementById('catch').style.marginTop = '15px'
+                                        document.getElementById('bloco3').style.height = '1050px'  
+                                    }else{
+
+                                        /*so consumir a api a partir daqui*/
+                                        /*as imagens ficam salva no localstorage */
+                                        let residenciaBase6 = window.localStorage.getItem('arquivoresidencia')
+                                        let camisaBase6 = window.localStorage.getItem('arquivocamisa')
+                                        let comdocBase6 = window.localStorage.getItem('arquivodocumento')
+    
+                                        let residenciaBase64 = residenciaBase6.replaceAll('/','>')
+                                        let camisaBase64 = camisaBase6.replaceAll('/','>')
+                                        let comdocBase64 = comdocBase6.replaceAll('/','>')
+                                    
+                                        document.getElementById('catch').innerHTML = 'Parabens, concluiu o registro'
+                                        document.getElementById('catch').style.color = 'green'
+                                        document.getElementById('catch').style.marginTop = '15px'
+                                        document.getElementById('bloco3').style.height = '1050px'
+                                        clearCamps()
+                                    }
+                                }
+                            }
+                        }
+                    }else{
+                        document.getElementById('catch').innerHTML = "Sua confirmação de senha está incoerente!"
+                        document.getElementById('catch').style.color = 'red'
+                        document.getElementById('catch').style.marginTop = '15px'
+                        document.getElementById('bloco3').style.height = '1050px'  
+                    }
+                }
+            }
+        }
     }
 }
